0d607d5d816c69cf86bfc437eaf22b3727ebe340
John Tucker <john.tucker@sharpspring.com>
2018-09-02 12:36:45 -0400

    Validator

--- src/todosManager.ts
+++ src/todosManager.ts
@@ -1,4 +1,5 @@
-import { Request, Response } from 'express';
+import { validate } from 'class-validator';
+import { NextFunction, Request, Response } from 'express';
 import { getConnection, Repository } from 'typeorm';
 import Todo from './entity/Todo';
 
@@ -9,28 +10,48 @@ const initialize = () => {
   repository = connection.getRepository(Todo);
 };
 
-export const createTodo = async (_: Request, res: Response) => {
+export const createTodo = async (_: Request, res: Response, next: NextFunction) => {
   if (repository === undefined) {
     initialize();
   }
-  const todo = new Todo();
-  todo.name = 'A Todo';
-  await repository.save(todo);
-  res.send(todo);
+  try {
+    const todo = new Todo();
+    todo.name = 'A Todo';
+    const errors = await validate(todo);
+    if (errors.length > 0) {
+      throw 400;
+    }
+    await repository.save(todo);
+    res.send(todo);
+  } catch (error) {
+    if (error === 400) {
+      res.status(400).send('Bad Request');
+    } else {
+      next(error);
+    }
+  }
 };
 
-export const readTodos = async (_: Request, res: Response) => {
+export const readTodos = async (_: Request, res: Response, next: NextFunction) => {
   if (repository === undefined) {
     initialize();
   }
-  const todos = await repository.find();
-  res.send(todos);
+  try {
+    const todos = await repository.find();
+    res.send(todos);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const readTodosIncomplete = async (_: Request, res: Response) => {
+export const readTodosIncomplete = async (_: Request, res: Response, next: NextFunction) => {
   if (repository === undefined) {
     initialize();
   }
-  const todos = await repository.find({ isComplete: false });
-  res.send(todos);
+  try {
+    const todos = await repository.find({ isComplete: false });
+    res.send(todos);
+  } catch (error) {
+    next(error);
+  }
 };
